30c33aa030fc5d25d3bd3a3996941fd7399e0fb0
Magnus <97634880+MagnusS0@users.noreply.github.com>
2023-12-11 15:42:01 +0000

    Feat: added new test for validation class
    - Doubbled check all issues, passed unit tests

--- submission/snumpy_test.py
+++ submission/snumpy_test.py
@@ -12,7 +12,8 @@ class TestSNumPy(unittest.TestCase):
         self.assertEqual(SNumPy.zeros(3), [0, 0, 0])
 
     def test_reshape(self):
-        self.assertEqual(SNumPy.reshape([1, 2, 3, 4], (2, 2)), [[1, 2], [3, 4]])
+        self.assertEqual(SNumPy.reshape(
+            [1, 2, 3, 4], (2, 2)), [[1, 2], [3, 4]])
         with self.assertRaises(ValueError):
             SNumPy.reshape([1, 2, 3], (2, 2))
 
@@ -22,7 +23,8 @@ class TestSNumPy(unittest.TestCase):
 
     def test_append(self):
         self.assertEqual(SNumPy.append([1, 2], [3, 4]), [1, 2, 3, 4])
-        self.assertEqual(SNumPy.append([[1], [2]], [[3], [4]], axis=1), [[1, 3], [2, 4]])
+        self.assertEqual(SNumPy.append(
+            [[1], [2]], [[3], [4]], axis=1), [[1, 3], [2, 4]])
 
     def test_get(self):
         self.assertEqual(SNumPy.get([[1, 2], [3, 4]], (0, 1)), 2)
@@ -31,35 +33,40 @@ class TestSNumPy(unittest.TestCase):
 
     def test_add(self):
         self.assertEqual(SNumPy.add([1, 2], [3, 4]), [4, 6])
-        self.assertEqual(SNumPy.add([[1, 2], [3, 4]], [[5, 6], [7, 8]]), [[6, 8], [10, 12]])
+        self.assertEqual(SNumPy.add([[1, 2], [3, 4]], [
+                         [5, 6], [7, 8]]), [[6, 8], [10, 12]])
 
     def test_subtract(self):
         self.assertEqual(SNumPy.subtract([4, 6], [1, 2]), [3, 4])
-        self.assertEqual(SNumPy.subtract([[6, 8], [10, 12]], [[1, 2], [3, 4]]), [[5, 6], [7, 8]])
+        self.assertEqual(SNumPy.subtract([[6, 8], [10, 12]], [
+                         [1, 2], [3, 4]]), [[5, 6], [7, 8]])
 
     def test_dotproduct(self):
         self.assertEqual(SNumPy.dotproduct([1, 2], [3, 4]), 11)
-        self.assertEqual(SNumPy.dotproduct([[1, 2], [3, 4]], [[5, 6], [7, 8]]), [[19, 22], [43, 50]])
+        self.assertEqual(SNumPy.dotproduct([[1, 2], [3, 4]], [
+                         [5, 6], [7, 8]]), [[19, 22], [43, 50]])
 
     def test_scalar_multiply(self):
         self.assertEqual(SNumPy.scalar_multiply([1, 2, 3], 3), [3, 6, 9])
-        self.assertEqual(SNumPy.scalar_multiply([[1, 2], [3, 4]], 2), [[2, 4], [6, 8]])
+        self.assertEqual(SNumPy.scalar_multiply(
+            [[1, 2], [3, 4]], 2), [[2, 4], [6, 8]])
 
     def test_aug_matrix(self):
-        self.assertEqual(SNumPy.aug_matrix([[1, 2], [3, 4]], [5, 6]), [[1, 2, 5], [3, 4, 6]])
+        self.assertEqual(SNumPy.aug_matrix(
+            [[1, 2], [3, 4]], [5, 6]), [[1, 2, 5], [3, 4, 6]])
         with self.assertRaises(ValueError):
             SNumPy.aug_matrix([[1, 2], [3, 4]], [5])
 
     def test_gaussian_elimination(self):
-        result =(SNumPy.gaussian_elimination([[2, 1], [5, 7]], [11, 13]))
+        result = (SNumPy.gaussian_elimination([[2, 1], [5, 7]], [11, 13]))
         self.assertAlmostEqual(result[0], 7.1, places=1)
         self.assertAlmostEqual(result[1], -3.2, places=1)
         with self.assertRaises(ValueError):
             SNumPy.gaussian_elimination([[1, 2], [2, 4]], [5, 10])
         with self.assertRaises(ValueError):
             SNumPy.gaussian_elimination([[1, 2]], [5, 10])
-        
-    
+
+
 class TestSNumPyValidation(unittest.TestCase):
 
     def test_invalid_vector_input(self):
@@ -80,24 +87,87 @@ class TestSNumPyValidation(unittest.TestCase):
         with self.assertRaises(ValueError):
             Validator.is_vector_or_matrix([[1, 2], [3]])  # Irregular shape
 
-    def test_shape_validation_for_operations(self):
+    def test_validate_shape_for_append(self):
+        # Test valid cases
+        self.assertIsNone(Validator.validate_shape_for_append(
+            [[1, 2]], [[3, 4]], 0))  # Valid matrix append row-wise
+        self.assertIsNone(Validator.validate_shape_for_append(
+            [[1], [2]], [[3], [4]], 1))  # Valid matrix append column-wise
+
+        # Test invalid cases
         with self.assertRaises(ValueError):
-            Validator.validate_shape_for_operations([1, 2], [[3, 4]], 'append')  # Vector and matrix mismatch
+            Validator.validate_shape_for_append(
+                [[1, 2]], [[3]], 0)  # Different matrix column counts
         with self.assertRaises(ValueError):
-            Validator.validate_shape_for_operations([1, 2], [3, 4, 5], 'add')  # Different vector sizes
-        # Add more cases for different operations
+            Validator.validate_shape_for_append(
+                [[1, 2]], [3, 4], 0)  # Vector to matrix
+
+    def test_validate_shape_for_add_subtract(self):
+        # Test valid cases
+        self.assertIsNone(Validator.validate_shape_for_add_subtract(
+            [1, 2], [3, 4]))  # Valid vector addition/subtraction
+        self.assertIsNone(Validator.validate_shape_for_add_subtract(
+            [[1, 2]], [[3, 4]]))  # Valid matrix addition/subtraction
+
+        # Test invalid cases
+        with self.assertRaises(ValueError):
+            Validator.validate_shape_for_add_subtract(
+                [1, 2, 3], [4, 5])  # Different vector lengths
+        with self.assertRaises(ValueError):
+            Validator.validate_shape_for_add_subtract(
+                [[1, 2]], [[3, 4, 5]])  # Different matrix dimensions
+
+    def test_validate_shape_for_dotproduct(self):
+        # Test valid cases
+        self.assertIsNone(Validator.validate_shape_for_dotproduct(
+            [1, 2], [3, 4]))  # Valid vector dot product
+        self.assertIsNone(Validator.validate_shape_for_dotproduct(
+            [[1, 2]], [[3], [4]]))  # Valid matrix dot product
+
+        # Test invalid cases
+        with self.assertRaises(ValueError):
+            Validator.validate_shape_for_dotproduct(
+                [1, 2, 3], [4, 5])  # Different vector lengths
+        with self.assertRaises(ValueError):
+            Validator.validate_shape_for_dotproduct(
+                [[1, 2, 3]], [[4, 5]])  # Incompatible matrix dimensions
+
+    def test_validate_shape_for_gaussian_elemination(self):
+        # Test valid cases
+        self.assertIsNone(Validator.validate_shape_for_gaussian_elemination(
+            [[1, 2], [3, 4]], [5, 6]))
+        self.assertIsNone(Validator.validate_shape_for_gaussian_elemination(
+            [[1, 2, 3], [4, 5, 6], [7, 8, 9]], [4, 5, 6]))
+
+        # Test invalid cases
+        with self.assertRaises(ValueError):
+            Validator.validate_shape_for_gaussian_elemination(
+                [[1, 2], [3, 4]], [5, 6, 7])  # Incompatible dimensions
+        with self.assertRaises(ValueError):
+            Validator.validate_shape_for_gaussian_elemination(
+                [[1, 2], [3, 4, 3]], [5, 6])  # Not a square matrix
+        with self.assertRaises(ValueError):
+            Validator.validate_shape_for_gaussian_elemination(
+                [[1, 2]], [5, 6, 7])  # Wrong matrix input
+        with self.assertRaises(ValueError):
+            Validator.validate_shape_for_gaussian_elemination(
+                [[1, 2], [3, 4]], [[1, 2], [3, 4]])  # Wrong vector input
 
     def test_index_validation(self):
         with self.assertRaises(IndexError):
-            Validator.validate_index_for_get([[1, 2], [3, 4]], (1, 3))  # Out of bounds
+            Validator.validate_index_for_get(
+                [[1, 2], [3, 4]], (1, 3))  # Out of bounds
         with self.assertRaises(ValueError):
-            Validator.validate_index_for_get([[1, 2], [3, 4]], '1,2')  # Invalid index format
+            Validator.validate_index_for_get(
+                [[1, 2], [3, 4]], '1,2')  # Invalid index format
 
     def test_reshape_validation(self):
         with self.assertRaises(ValueError):
-            Validator.validate_shape_for_reshape([[1, 2], [3, 4]], (2, 2))  # Not a vector
+            Validator.validate_shape_for_reshape(
+                [[1, 2], [3, 4]], (2, 2))  # Not a vector
         with self.assertRaises(ValueError):
-            Validator.validate_shape_for_reshape([1, 2, 3, 4], (3, 2))  # Incompatible shape
+            Validator.validate_shape_for_reshape(
+                [1, 2, 3, 4], (3, 2))  # Incompatible shape
 
 
 if __name__ == '__main__':
